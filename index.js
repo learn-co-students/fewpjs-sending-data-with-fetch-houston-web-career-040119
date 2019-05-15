44b8132837bd173ed49d57ea970ef8c7054e0656
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-15 13:40:38 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+
+function submitData(name,email) {
+  let headers = {"Content-Type":"application/json",
+    "Accept":"application/json"
+  };
+
+  let formData = { "name": name, "email": email };
+
+  return fetch("http://localhost:3000/users",
+  {
+    method:"POST",
+    headers:headers,
+    body:JSON.stringify(formData)
+  }).then(function(res) {
+    return res.json();
+  }).then(function(data) {
+    let pElem = document.createElement("p");
+    document.body.appendChild(pElem);
+    pElem.innerHTML = data.id;
+  }).catch(function(error) {
+    let pElem = document.createElement("p");
+    document.body.appendChild(pElem);
+    pElem.innerHTML = error.message;
+  });
+}
