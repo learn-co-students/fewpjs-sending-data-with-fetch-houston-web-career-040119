cbcc861a4061ca5aa913fd71acf151d1de12c6ad
shawn0527 <shawnren527@gmail.com>
2019-05-15 15:23:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+function submitData(name, email){
+    let formData = {
+        name: name,
+        email: email
+    }
+
+    let configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+        // let div = document.createElement('div')
+        let p = document.createElement('p')
+        // debugger
+        p.innerHTML = data.id
+        // div.append(p)
+        document.body.append(p)
+    })
+    .catch(function(error){
+        alert('Unauthorized Access')
+        // let div = document.createElement('div')
+        let p = document.createElement('p')
+        p.innerHTML = error.message
+        // div.append(p)
+        document.body.append(p)
+    })
+}
+    
