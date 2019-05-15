315da8ba61a5426adb1df8f1d368018e87fde200
jpavioli <justin.avioli@gmail.com>
2019-05-15 14:29:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name,email){
+
+  let formData = {
+    name:name,
+    email:email
+  }
+
+  let configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json",
+      "Accept":"application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users",configObject)
+    .then(function(res){
+      return res.json()
+    })
+    .then(function(object){
+      let p = document.createElement('p')
+      document.body.appendChild(p)
+      p.innerHTML = object.id
+    })
+    .catch(function(error){
+      let p = document.createElement('p')
+      document.body.appendChild(p)
+      p.innerHTML = error.message
+    })
+
+}
