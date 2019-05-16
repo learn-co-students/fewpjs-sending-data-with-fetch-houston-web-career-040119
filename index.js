0bb9e417c305d1d67ddeecb24e91f4790f7aca12
EC2 Default User <ec2-user@ip-172-31-33-230.us-east-2.compute.internal>
2019-05-16 02:47:08 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(userName,userEmail){
+    
+    let formData={
+        name: userName,
+        email: userEmail
+    };
+    
+    let configObj={
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify(formData)
+    };
+    
+    
+    return window.fetch("http://localhost:3000/users",configObj)
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(obj){
+        document.body.append(obj.id);
+    })
+    .catch(function(err){
+        document.body.append(err.message)
+    })
+}   
