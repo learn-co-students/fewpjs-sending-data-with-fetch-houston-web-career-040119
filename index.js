207db7aa4d72f94531285a8a5465fabd78f40a22
PYN <yxp7531540@gmail.com>
2019-05-18 19:13:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(username, email) {
+    let url = "http://localhost:3000/users"
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: username,
+            email: email
+        })
+    }
+    return fetch(url, configObj)
+        .then(resp => resp.json())
+        .then(obj => {
+            document.write(obj.id)
+        })
+        .catch((error) => {
+            document.write(error.message)
+        })
+}
